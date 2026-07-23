93e1294305839885905b6692ce3954f06a538dc8
udit <uditpatil82@gmail.com>
2026-07-23 19:12:42 +0530

    done with basics

---
+++ src/Basics.java
@@ -0,0 +1,95 @@
+//public class Basics {
+//    public static void main(String[] args) {
+//        System.out.println("hello java");
+//    }
+//}
+
+// SUM OF 2 NUMBERS
+//public class Basics{
+//    public static void main(String[] args) {
+//        int a = 23;
+//        int b = 34;
+//        int sum =  a + b ;
+//        System.out.println(sum);
+//    }
+//}
+
+//public class Basics{
+//    public static void main(String[] args) {
+//        int a = 23;
+//        float b = 34f;
+//        float sum =  a + b ;
+//        System.out.println(sum);
+//    }
+//}
+
+//TYPE CONVERSION
+
+//IMPLICIT
+
+//Q1. Convert an int value 45 to double.
+//public class Basics {
+//    public static void main(String[] args) {
+//        int a = 45;
+//        double b = a;
+//        System.out.println(b);
+//    }
+//}
+
+//Q2. Convert a char value 'C' to int.
+//public class Basics {
+//    public static void main(String[] args) {
+//        char c = 'C';
+//        int d  = c;
+//        System.out.println(d);
+//    }
+//}
+
+//Q3. Convert a byte value 20 to long.
+//public class Basics {
+//    public static void main(String[] args) {
+//        byte e = 20 ;
+//        long f  = e;
+//        System.out.println(f);
+//    }
+//}
+
+//Q4. Convert an int value 100 to float.
+//public class Basics {
+//    public static void main(String[] args) {
+//        int g = 100 ;
+//        float h = g;
+//        System.out.println(h);
+//    }
+//}
+
+//EXPLICIT
+
+//Q5. Convert a double value 15.75 to int (using explicit casting). What will the result be?
+//public class Basics {
+//    public static void main(String[] args) {
+//        double i = 15.75 ;
+//        int j = (int) i;
+//        System.out.println(j);
+//    }
+//}
+
+//6. Convert a float value 9.5f to int (using explicit casting
+//public class Basics {
+//    public static void main(String[] args) {
+//        float k = 9.6f ;
+//        int l = (int) k;
+//        System.out.println(l);
+//    }
+//}
+
+//Q7. Convert a long value 500000L to int
+public class Basics {
+    public static void main(String[] args) {
+        long m = 500000L ;
+        int n = (int) m;
+        System.out.println(n);
+    }
+}
+
+
