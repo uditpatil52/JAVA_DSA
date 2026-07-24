94e7625607a76d150f1a8e2d491b23238b51d9db
udit <uditpatil82@gmail.com>
2026-07-24 21:51:32 +0530

    done with operators

---
+++ src/Operators.java
@@ -0,0 +1,40 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        //ARITMETIC OP
+//     int a = 65 ;
+//     int b = 5;
+//        System.out.println(a/b);
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(b%a);
+
+        //RELATIONAL OP
+//        int a = 5;
+//        int b = 6;
+//        System.out.println(a>b);
+//        System.out.println(a==b);
+//        System.out.println(a<b);
+
+        //logical op
+//        boolean result = (5>3)&&(4>7);
+//        boolean result1 = (5>3)&&(4<7);
+//        System.out.println(result);
+//        System.out.println(result1);
+
+//        boolean result2 = (5>3)||(4>7);
+//        boolean result3 = (5>3)||(4<7);
+//        boolean result4 = (5<3)||(4>7);
+//        System.out.println(result2);
+//        System.out.println(result3);
+//        System.out.println(result4);
+
+        boolean result5 = !(2>4);
+        System.out.println(result5);
+
+
+
+    }
+
+}
