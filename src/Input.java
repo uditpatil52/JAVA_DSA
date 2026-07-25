0c4e001dcc45857a29778392a24316d1c847c22a
udit <uditpatil82@gmail.com>
2026-07-25 19:37:45 +0530

    pratice with scanner input questions

--- src/Input.java
+++ src/Input.java
@@ -1,17 +1,60 @@
+//import java.util.Scanner;
+//
+//public class Input {
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//
+//        System.out.println("Enter your name: ");
+//        String name = inp.nextLine();
+//
+//        System.out.println("Enter your age: ");
+//        int age = inp.nextInt();
+//
+//        System.out.println("Enter your percentage: ");
+//        float per = inp.nextFloat();
+//    }
+//
+//}
+
+
+//Write a program that takes two numbers from the user (as int), and prints their sum.
+//import java.util.Scanner;
+//
+//public class Input {
+//    public static void main(String[] args) {
+//        Scanner opt = new Scanner(System.in);
+//
+//        System.out.println("enter 1st num : ");
+//        int num1 = opt.nextInt();
+//
+//        System.out.println("enter 2nd num : ");
+//        int num2 = opt.nextInt();
+//
+//        int sum = num1 + num2;
+//        System.out.println(sum);
+//    }
+//}
+
+
 import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+        Scanner op = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name = inp.nextLine();
+        System.out.print("enter your name :");
+        String name = op.nextLine();
 
-        System.out.println("Enter your age: ");
-        int age = inp.nextInt();
+        System.out.print("enter your marks : ");
+        double marks = op.nextDouble();
 
-        System.out.println("Enter your percentage: ");
-        float per = inp.nextFloat();
+        System.out.println("name: "  + name + ", Marks : " + marks);
     }
-
 }
+//
+//Predict the output of this code:
+//
+//int a = 4;
+//int b = 6;
+//System.out.println("Result: " + a + b); //46
+//System.out.println("Result: " + (a + b));//10
