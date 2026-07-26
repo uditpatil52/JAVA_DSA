963556e8e5b629d230b99f02492707468dd894fe
udit <uditpatil82@gmail.com>
2026-07-26 15:10:40 +0530

    done with loops

---
+++ src/Loops.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        //Q1. Write a program using a for loop to print numbers from 1 to 10.
+//        for(int i = 1;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+        //Q2. Write a program using a while loop to print numbers from 10 down to 1.
+//        int i = 10;
+//        while(i>=1){
+//            System.out.println(i);
+//            i--;
+//        }
+
+//        //Q3. Write a program using a for loop to print all even numbers between 1 and 20.
+//        for(int i = 1 ; i<=20;i++){
+//            if(i%2==0){
+//                System.out.println(i);
+//            }
+//        }
+
+        //Q4. Write a program to find the sum of first N natural numbers (take N as input) using a for loop.
+//        Scanner inp = new Scanner(System.in);
+//
+//        System.out.println("enter your number : ");
+//        int N = inp.nextInt();
+//
+//       int sum = 0;
+//
+//        for(int i=1 ; i<=N; i++){
+//            sum = sum + i;
+//            System.out.println(sum);
+//        }
+//        System.out.println("total sum : " + sum);
+
+//        //Q5. Write a program using a do-while loop to print numbers from 1 to 5.
+//        int i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i<=5);
+
+        //Q8. Write a program using a for loop to print the multiplication table of a number (say 5), from 1 to 10.
+
+//        int num = 5;
+//
+//        for ( int i = 1; i<=10 ;i++){
+//            System.out.println(num + " x"  + i + " =" + (num * i)) ;
+//        }
+
+    }
+}
