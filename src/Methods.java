78d2279635b0c7deb27fd1934df9e64248c7b7e9
udit <uditpatil82@gmail.com>
2026-08-01 19:26:22 +0530

    done with returning values and string

--- src/Methods.java
+++ src/Methods.java
@@ -1,19 +1,63 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class Methods {
-    public static void main(String[] args) {
-        //Q SUM OF TWO NUM
-       sum();                         //calling a function
-    }
+//public class Methods {
+//    public static void main(String[] args) {
+//      //Q SUM OF TWO NUM
+//       sum();                         //calling a function
+//   }
+//
+//    static void sum() {
+//        Scanner inp = new Scanner(System.in);
+//        System.out.print("enter first number: ");
+//        int num1 = inp.nextInt();
+//        System.out.print("enter second number: ");
+//        int num2 = inp.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("the sum is : " + sum);
+//
+//    }
+//}
 
-    static void sum() {
-        Scanner inp = new Scanner(System.in);
-        System.out.print("enter first number: ");
-        int num1 = inp.nextInt();
-        System.out.print("enter second number: ");
-        int num2 = inp.nextInt();
-        int sum = num1 + num2;
-        System.out.println("the sum is : " + sum);
 
-    }
-}
+
+//public class Methods {
+//    public static void main(String[] args) {
+//        greeting();
+//    }
+//    static void greeting(){
+//        System.out.println("Good Evening");
+//    }
+//
+//}
+
+//import java.util.Scanner;
+//
+//public class Methods {
+//    public static void main(String[] args) {
+//      //Q SUM OF TWO NUM
+//      int ans = sum();
+//        System.out.println(ans);
+//   }
+//
+//    static int sum() {
+//        Scanner inp = new Scanner(System.in);
+//        System.out.print("enter first number: ");
+//        int num1 = inp.nextInt();
+//        System.out.print("enter second number: ");
+//        int num2 = inp.nextInt();
+//        int sum = num1 + num2;
+//        return sum;               // it will be over , no more thing will execute now after this
+//
+//    }
+//}
+
+//public class Methods {
+//    public static void main(String[] args) {
+//        String greetMessage = greet();
+//        System.out.println(greetMessage);
+//    }
+//    static String greet(){
+//        String greeting = " hey java ";
+//        return greeting;
+//    }
+//}
